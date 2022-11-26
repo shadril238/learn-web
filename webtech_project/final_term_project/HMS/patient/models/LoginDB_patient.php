52632a2a22bd0b104a3600b513118a6485731b07
shadril238 <shadrilhassan@outlook.com>
2022-11-27 03:33:06 +0600

    nothing changed

--- webtech_project/final_term_project/HMS/patient/models/LoginDB_patient.php
+++ webtech_project/final_term_project/HMS/patient/models/LoginDB_patient.php
@@ -6,7 +6,7 @@
             die("Connection failed: " . mysqli_connect_error());
         }
         $stmt = mysqli_stmt_init($conn);
-        $sql = "SELECT p_id, p_email, p_pass FROM patient_data WHERE p_email = ? and p_pass = ?";
+        $sql = "SELECT p_id, p_email, p_pass FROM patient_data WHERE p_email = ? AND p_pass = ?";
         mysqli_stmt_prepare($stmt, $sql);
         mysqli_stmt_bind_param($stmt, "ss", $email, $password);
         mysqli_stmt_execute($stmt);
